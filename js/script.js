afbad196efe702fba97a2cf29b40d09a5723972f
Bruno Pessoa <brunopessoa.melo@gmail.com>
2020-04-22 22:06:54 -0300

    subindo o projeto, folha de estilo e pagina criados

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.getElementById("cart-info").addEventListener
+("click", function(){
+	const cart = document.getElementById("cart");
+	cart.classList.toogle("show-cart");
+	console.log(cart);
+});
